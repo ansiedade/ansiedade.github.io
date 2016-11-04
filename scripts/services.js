5efd0a71bfa589635aab7c3c6a93d22787281bda
Josmario <josmarios.silva@gmail.com>
2016-11-04 17:59:04 -0300

    post request changed

--- scripts/services.js
+++ scripts/services.js
@@ -83,20 +83,37 @@ tutorServices.service("User", function($http) {
     };
 
     this.save = function() {
-        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        // $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
-        $http({
-            url: "http://162.243.222.205:8282/back/CapstoneServlet",
-            method: "POST",
-            data: JSON.stringify(response)
-        }).then(function(response) {
-            // success
-            console.log("Response sent!");
+        // $http({
+        //     url: "http://162.243.222.205:8282/back/CapstoneServlet",
+        //     method: "POST",
+        //     data: JSON.stringify(response)
+        // }).then(function(response) {
+        //     // success
+        //     console.log("Response sent!");
 
-        }, function(response) {
-            // failed
-            console.error("Failed on submitting answer. " + response);
-        });
+        // }, function(response) {
+        //     // failed
+        //     console.error("Failed on submitting answer. " + response);
+        // });
+
+        //-------------------------
+
+        var http = new XMLHttpRequest();
+        var url = "http://162.243.222.205:8282/back/CapstoneServlet";
+        var data = JSON.stringify(response);
+        http.open("POST", url, true);
+
+        //Send the proper header information along with the request
+        http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        http.onreadystatechange = function() { //Call a function when the state changes.
+            if (http.readyState == 4 && http.status == 200) {
+                console.log(http.responseText);
+            }
+        }
+        http.send(data);
     };
 
 });
